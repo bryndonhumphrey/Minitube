69b52817a250e66680681e106bfcee51a25296e6
bryndonhumphrey <64496396+bryndonhumphrey@users.noreply.github.com>
2020-07-22 11:16:34 -0700

    Updates

---
+++ precache-manifest.5b4b46f448fae3b1125a8c4a1bd630cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e1e7f1a349cb9bcf26056b41f740a7",
+    "url": "/Videos-using-hooks/index.html"
+  },
+  {
+    "revision": "63ad4cd7b78530ced60d",
+    "url": "/Videos-using-hooks/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "96ad82ab4fa657787d52",
+    "url": "/Videos-using-hooks/static/js/2.dbfe14b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Videos-using-hooks/static/js/2.dbfe14b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ad4cd7b78530ced60d",
+    "url": "/Videos-using-hooks/static/js/main.2224467c.chunk.js"
+  },
+  {
+    "revision": "f46a30a97180897f72fb",
+    "url": "/Videos-using-hooks/static/js/runtime-main.0ae7e049.js"
+  }
+]);
